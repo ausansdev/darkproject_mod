bb8c668923ff328e6fedd960f780c827eec105d2
sansdev <89143494+ausansdev@users.noreply.github.com>
2024-03-19 18:16:52 +0300

    fd

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,7 @@ using namespace geode::prelude;
 class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
-
-        this->removeAllChildrens();
-
+        
         auto label = CCLabelBMFont::create("darkproject v1.1", "bigFont.fnt");
         label->setPosition(170, 60);
         this->addChild(label);
